5a87ee9f83a990d74591f52ebc23733282460d42
sdxsd <willmaguire@icloud.com>
2023-01-15 12:47:55 +0100

    Removed Codam header.

--- src/ft_vector_operations.c
+++ src/ft_vector_operations.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        ::::::::            */
-/*   CODAM C FILE                                       :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: wmaguire <wmaguire@student.codam.nl>         +#+                     */
-/*                                                   +#+                      */
-/*   Created: 1970/01/01 00:00:00 by wmaguire      #+#    #+#                 */
-/*   Updated: 1970/01/01 00:00:00 by wmaguire     ########   codam.nl         */
-/*                                                                            */
-/* ************************************************************************** */
-
 /*
 THIS FILE IS LICENSED UNDER THE GNU GPLv3
 Copyright (C) 2022  Will Maguire
